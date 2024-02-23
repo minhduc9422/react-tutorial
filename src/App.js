4aaf8c6d37d8b3d866760f66a42b53ee5adb630a
Phạm Minh Đức <minhduc9422@gmail.com>
2024-02-23 15:29:28 +0700

    create first state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+let stateValue;
+
+function useState(initialState) {
+  if (stateValue === undefined) {
+    stateValue = initialState;
+  }
+
+  function setValue(value) {
+    stateValue = value;
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+
+  return [stateValue, setValue];
+}
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrease = function () {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrease = function () {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 }
